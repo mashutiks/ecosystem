4253b091a0dd7dc907ec06835930732a8358d731
snsloe <157033827+snsloe@users.noreply.github.com>
2024-04-03 02:25:16 +0300

    Create adding.h

---
+++ sourse/adding.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "capybara.h"
+#include "cayman.h"
+#include "orange.h"
+#include "defines.h"
+#include <vector>
+
+void add_capybara(std::vector<capybara>& capybaras);
+
+void add_cayman(std::vector<cayman>& caymans);
+
+void add_orange(std::vector<orange>& oranges);
